e65086efdbabceee2f632e7e9088cf9d29baf6a7
t.clar <t.clar@rocketlab.co.nz>
2022-05-11 18:13:03 +1200

    cube map implementation

---
+++ general_inc/cube_map.h
@@ -0,0 +1,161 @@
+#include <Eigen/Core>
+
+#include <vector>
+#include <stdexcept>
+
+#include <QOpenGLContext> 
+#include <QOpenGLFunctions_3_3_Core>
+
+#include <general_inc/shader.h>
+
+float skybox_vertices[] = {
+    // positions          
+    -1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+
+    -1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+
+    -1.0f, -1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+
+    -1.0f,  1.0f, -1.0f,
+        1.0f,  1.0f, -1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f,
+
+    -1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+        1.0f, -1.0f,  1.0f
+};
+
+
+class CubeMap: protected QOpenGLFunctions_3_3_Core
+{
+public:
+    
+    CubeMap() = delete; // need to at least give the filenames
+    CubeMap(std::string cube_map_path){
+
+        // Point shader
+        const char* vertex_shader_path = "/home/t.clar/Repos/openGLQt/shaders/cubemap.vs";
+        const char* fragment_shader_path = "/home/t.clar/Repos/openGLQt/shaders/cubemap.fs";
+        m_cubemap_shader = new Shader(vertex_shader_path, fragment_shader_path);
+
+        initializeOpenGLFunctions();   // Initialise current context  (required)
+
+        setup();  // Setup VBO and VAO 
+
+        // Cube map images need to have following names
+        vector<std::string> faces
+        {
+            "/home/t.clar/Repos/openGLQt/resources/cubemaps/opensky/right.jpg",
+            "/home/t.clar/Repos/openGLQt/resources/cubemaps/opensky/left.jpg",
+            "/home/t.clar/Repos/openGLQt/resources/cubemaps/opensky/top.jpg",
+            "/home/t.clar/Repos/openGLQt/resources/cubemaps/opensky/bottom.jpg",
+            "/home/t.clar/Repos/openGLQt/resources/cubemaps/opensky/front.jpg",
+            "/home/t.clar/Repos/openGLQt/resources/cubemaps/opensky/back.jpg"
+        };
+        
+        // Load texture 
+        m_cubemap_texture = load_cube_map(faces);  
+    } 
+
+
+    unsigned int load_cube_map(vector<std::string> faces)
+    {
+        unsigned int textureID;
+        glGenTextures(1, &textureID);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+        stbi_set_flip_vertically_on_load(false);
+        int width, height, nrChannels;
+        for (unsigned int i = 0; i < faces.size(); i++)
+        {
+            unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+            if (data)
+            {
+                glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 
+                            0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+                );
+                stbi_image_free(data);
+            }
+            else
+            {
+                std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+                stbi_image_free(data);
+            }
+        }
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+        return textureID;
+    } ;
+
+    void setup()
+    {
+        glGenVertexArrays(1, &vao_);
+        glGenBuffers(1, &vbo_);
+        glBindVertexArray(vao_);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(skybox_vertices), &skybox_vertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    }
+
+    void draw(glm::mat4 view_matrix = glm::mat4(1.0f), glm::mat4 projection_matrix = glm::mat4(1.0f))
+    {
+        // draw skybox as last
+        glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+        
+        m_cubemap_shader->use();
+        m_cubemap_shader->setInt("skybox", 0);
+
+        glm::mat4 view = glm::mat4(glm::mat3(view_matrix)); // remove translation from the view matrix
+        m_cubemap_shader->setMat4("view", view);
+        m_cubemap_shader->setMat4("projection", projection_matrix);
+
+        // Draw skybox cube
+        glBindVertexArray(vao_);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, m_cubemap_texture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthFunc(GL_LESS); // set depth function back to default
+    }
+
+    private:
+        unsigned int vao_, vbo_;
+        unsigned int m_cubemap_texture;
+
+        Shader* m_cubemap_shader;
+
+
+};
